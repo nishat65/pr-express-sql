cdf681977e022e24de4214c521f8c40f83f5002e
nishat65 <nishatroy5@gmail.com>
2024-04-29 18:20:13 +0530

    create menuitem test

--- test/menitem.test.js
+++ test/menitem.test.js
@@ -1,16 +1,10 @@
+const path = require('path');
 const request = require('supertest');
 const app = require('../src');
 const { connectDB, db } = require('../src/utils/db');
 
 beforeAll(async () => {
     await connectDB();
-});
-afterEach(() => {
-    jest.clearAllMocks();
-});
-afterAll(async () => {
-    await db.sync({ force: true });
-    await db.close();
 });
 
 test('get all menuitems', async () => {
@@ -21,4 +15,30 @@ test('get all menuitems', async () => {
 test('get error when getting menuitem by id', async () => {
     const response = await request(app).get('/api/v1/menuitems/1');
     expect(response.statusCode).toBe(404);
+});
+
+test('create menuitem', async () => {
+    try {
+        const imagePath = path.resolve(__dirname, '../public/uploads/test.jpg');
+        const response = await request(app)
+            .post('/api/v1/menuitems/')
+            .field('name', 'test')
+            .field('description', 'test')
+            .field('price', 10.0)
+            .field('type', 'veg')
+            .field('mealType', 'lunch')
+            .field('category', 'main')
+            .attach('image', imagePath);
+        expect(response.statusCode).toBe(201);
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
+});
+afterAll(async () => {
+    await db.sync({ force: true });
+    await db.close();
 });
